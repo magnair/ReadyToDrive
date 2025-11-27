434a03dabf531c2738daeb428da83ea1567c411b
Ola Eriksen <Ola.Eriksen@bouvet.no>
2025-11-27 12:36:17 +0100

    Fixed package

--- src/components/AlcoCalc.tsx
+++ src/components/AlcoCalc.tsx
@@ -191,7 +191,7 @@ export default function AlcoCalc({ darkMode }: AlcoCalcProps) {
                     color: '#fff !important',
                     WebkitTextFillColor: '#fff !important'
                   }
-                }}              
+                }}
                 disabled
               />
               <IconButton
